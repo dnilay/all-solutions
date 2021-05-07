3c057610732c0ee385105295689c54b562983dd2
Nilay <t-nilay1@cognizant.com>
2021-05-07 11:50:18 +0530

    rectified bugs

---
+++ item-service/src/main/java/com/example/demo/service/ItemBusinessService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.Item;
+import com.example.demo.repo.ItemRepository;
+
+@Component
+public class ItemBusinessService {
+	
+	@Autowired
+	private ItemRepository repository;
+	
+	public Item retreiveHardcodedItem() {
+		return new Item(1, "Ball", 10, 100);
+	}
+	
+	public List<Item> retrieveAllItems() {
+		List<Item> items = repository.findAll();
+		
+		for(Item item:items) {
+			item.setValue(item.getPrice() * item.getQuantity());
+		}
+		
+		return items;	
+	}
+	
+	public Optional<Item> find(Integer id)
+	{
+		return repository.findById(id);
+	}
+	
+}
